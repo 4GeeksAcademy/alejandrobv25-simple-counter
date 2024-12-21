1d078bdfe23ea064e7850e8b7e328d968da3d4e9
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-12-21 14:40:20 +0000

    first commit

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Counter = () => {
+    let [horas, minutos, segundos] = [0, 0, 0]
+    function actualizarCronometro() {
+        segundos++;
+        if (segundos / 60 === 1) {
+            segundos = 0;
+            minutos++;
+
+            if (minutos / 60 === 1) {
+                minutos = 0
+                horas++;
+            }
+        }
+        const segundosUnaCifra = asignarCifra(segundos);
+        const minutosUnaCifra = asignarCifra(minutos);
+        const horasUnaCifra = asignarCifra(horas);
+        document.getElementById("crono").innerText = `${horasUnaCifra}:${minutosUnaCifra}:${segundosUnaCifra}`
+    }
+    function asignarCifra(unidadDeTiempo) {
+        return unidadDeTiempo < 10 ? "0" + unidadDeTiempo : unidadDeTiempo;
+    }
+    setInterval(actualizarCronometro, 1000);
+    return(
+        <div className="contenedor-principal">
+            <div className="counter-container" id='crono'>
+                00:00:00
+
+            </div>
+        </div>
+    )
+}
+
